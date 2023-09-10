e54289e1566a257b398f9cda6a729ae29b61e51b
Matias Lapolla <matias.lapolla.1@gmail.com>
2023-09-10 02:47:50 -0300

    refactored signin page

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -1,31 +1,40 @@
-import { Grid, Link, Typography } from '@mui/material'
-import React from 'react'
+import { Grid, Link, Typography } from '@mui/material';
+import React from 'react';
 
 const FooterComponent = () => {
-  return (
-    <>
-      <Grid display={"flex"} flexDirection={"column"} alignItems={"center"} sx={{
-        fontSize: "1.5rem",
-        fontWeight: "normal",
-        fontFamily: "Roboto",
-        color: "grey",
-        borderTop: "1px solid lightgrey",
-      }} minHeight={64} justifyContent={"center"}>
-        <Link
-          sx={{
-            color: "grey",
-            textDecoration: "none",
-            fontWeight: "bold",
-            "&:hover": {
-              color: "darkblue",
-              transition: "all 0.3s ease-in-out",
-            }
-          }
-          }
-          href="https://www.linkedin.com/in/matiaslapolla/" target='_blank' rel="noreferrer">Matias Lapolla - LinkedIn</Link>
-      </Grid>
-    </>
-  )
-}
+	return (
+		<>
+			<Grid
+				display={'flex'}
+				flexDirection={'column'}
+				alignItems={'center'}
+				sx={{
+					fontSize: '1.5rem',
+					fontWeight: 'normal',
+					fontFamily: 'Roboto',
+					color: 'grey',
+					borderTop: '1px solid lightgrey',
+				}}
+				minHeight={64}
+				justifyContent={'center'}>
+				<Link
+					sx={{
+						color: 'grey',
+						textDecoration: 'none',
+						fontWeight: 'bold',
+						'&:hover': {
+							color: 'darkblue',
+							transition: 'all 0.3s ease-in-out',
+						},
+					}}
+					href='https://www.linkedin.com/in/matiaslapolla/'
+					target='_blank'
+					rel='noreferrer'>
+					Matias Lapolla - LinkedIn
+				</Link>
+			</Grid>
+		</>
+	);
+};
 
-export default FooterComponent
+export default FooterComponent;
