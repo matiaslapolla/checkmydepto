c6cdb8d2769180b87101aa9a7f58a6308197439f
Matias Lapolla <matias.lapolla.1@gmail.com>
2023-09-09 22:11:54 -0300

    updated contact page

--- src/Pages/Contact/index.js
+++ src/Pages/Contact/index.js
@@ -1,112 +1,209 @@
-import { Button, Grid, TextField, Typography } from '@mui/material'
-import React, { useState } from 'react'
+import { Button, Grid, TextField, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import getInTouch from '../../Assets/get_in_touch_pana.png';
 
 const textStyleObject = {
-  "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
-    borderColor: "orange",
-  },
-  "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
-    borderColor: "green",
-  },
-  "& .MuiOutlinedInput-input": {
-    color: "black",
-  },
-  "& .MuiInputLabel-outlined": {
-    color: "black",
-  },
-  "&:hover .MuiOutlinedInput-input": {
-    color: "black",
-  },
-  "&:hover .MuiInputLabel-outlined": {
-    color: "green",
-  },
-  "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-input": {
-    color: "green",
-  },
-  "& .MuiInputLabel-outlined.Mui-focused": {
-    color: "green",
-  },
-  "&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
-    borderColor: "green",
-  },
-}
-
+	'& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline': {
+		borderColor: 'orange',
+	},
+	'& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline': {
+		borderColor: 'green',
+	},
+	'& .MuiOutlinedInput-input': {
+		color: 'black',
+	},
+	'& .MuiInputLabel-outlined': {
+		color: 'black',
+	},
+	'&:hover .MuiOutlinedInput-input': {
+		color: 'black',
+	},
+	'&:hover .MuiInputLabel-outlined': {
+		color: 'green',
+	},
+	'& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-input': {
+		color: 'green',
+	},
+	'& .MuiInputLabel-outlined.Mui-focused': {
+		color: 'green',
+	},
+	'&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline': {
+		borderColor: 'green',
+	},
+};
 
 const Contact = () => {
+	const [contactForm, setContactForm] = useState({
+		name: '',
+		email: '',
+		subject: '',
+		message: '',
+	});
 
-  const [contactForm, setContactForm] = useState({
-    name: "",
-    email: "",
-    subject: "",
-    message: "",
-  })
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		alert('Message Sent');
+	};
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    alert("Message Sent")
-  }
+	const handleChange = (e) => {
+		setContactForm({
+			...contactForm,
+			[e.target.name]: e.target.value,
+		});
+	};
 
-  const handleChange = (e) => {
-    setContactForm({
-      ...contactForm,
-      [e.target.name]: e.target.value
-    })
-  }
+	return (
+		<Grid
+			container
+			direction='column'
+			justifyContent='center'
+			alignItems='center'
+			sx={{
+				backgroundImage: `url(${getInTouch})`,
+				backgroundRepeat: 'no-repeat',
+				backgroundPosition: 'center',
+				backgroundSize: 1000,
+				backgroundBlendMode: 'multiply',
+				minHeight: '100vh',
+			}}>
+			<Grid
+				container
+				alignItems={'center'}
+				justifyContent={'center'}
+				sx={{
+					backdropFilter: 'blur(16px)',
+					minHeight: '100vh',
+					minWidth: '100vw',
+					backgroundColor: '#FFFFFF75',
+				}}>
+				<Grid
+					container
+					flexDirection={{ xs: 'column', sm: 'row', md: 'row' }}
+					justifyContent={'center'}
+					alignItems={'center'}
+					maxWidth={{
+						xs: '100%',
+						sm: '80%',
+						md: '70%',
+					}}
+					gap='2rem'>
+					<Grid
+						container
+						maxWidth={{
+							xs: '85%',
+							md: '40%',
+						}}
+						flexDirection={'column'}
+						backgroundColor={'orange'}
+						padding={'2rem'}
+						borderRadius={'2rem'}>
+						<Typography
+							fontSize={'2rem'}
+							fontWeight={'bold'}
+							color={'black'}
+							gutterBottom>
+							Greetings from the team at CheckMyDepto!
+						</Typography>
+						<Typography
+							variant='body2'
+							fontSize={'1rem'}
+							sx={{ color: 'black' }}
+							letterSpacing={0.5}
+							gutterBottom>
+							We are here to help and answer any question you
+							might have. We look forward to hearing from you.
+						</Typography>
+						<Typography
+							variant='body2'
+							fontSize={'1rem'}
+							sx={{ color: 'black' }}
+							letterSpacing={0.5}
+							gutterBottom>
+							If you are a business owner and would like to be
+							featured on our website, please contact us as well.
+						</Typography>
+					</Grid>
+					<Grid
+						container
+						item
+						justifyContent={'center'}
+						maxWidth={{
+							xs: '85%',
+							md: '40%',
+						}}>
+						<form
+							onSubmit={handleSubmit}
+							style={{
+								display: 'flex',
+								width: '85%',
+							}}>
+							<Grid
+								container
+								gap={2}
+								flexDirection={'column'}
+								width={'100%'}>
+								<Grid
+									container
+									justifyContent={'space-between'}
+									gap={2}
+									flexDirection={'column'}>
+									<TextField
+										name='name'
+										onChange={handleChange}
+										fullWidth
+										placeholder='Name'
+										sx={textStyleObject}
+									/>
+									<TextField
+										name='email'
+										onChange={handleChange}
+										fullWidth
+										required
+										placeholder='Email'
+										sx={textStyleObject}
+									/>
+									<TextField
+										name='subject'
+										onChange={handleChange}
+										fullWidth
+										placeholder='Subject'
+										sx={textStyleObject}
+									/>
+								</Grid>
+								<Grid
+									container
+									gap={2}
+									justifyContent={'center'}>
+									<TextField
+										name='message'
+										onChange={handleChange}
+										fullWidth
+										required
+										placeholder='Message'
+										multiline
+										rows={4}
+										sx={textStyleObject}
+									/>
+									<Button
+										variant='contained'
+										type='submit'
+										size='large'
+										sx={{
+											backgroundColor: 'orange',
+											'&:hover': {
+												backgroundColor: 'green',
+											},
+										}}>
+										Send Message
+									</Button>
+								</Grid>
+							</Grid>
+						</form>
+					</Grid>
+				</Grid>
+			</Grid>
+		</Grid>
+	);
+};
 
-
-  return (
-    <Grid
-      container
-      direction="column"
-      justifyContent="center"
-      alignItems="center"
-      sx={{
-        backgroundColor: '#FF800035',
-        minHeight: '100vh',
-      }}
-    >
-      <Grid item>
-        <form onSubmit={handleSubmit}>
-          <Typography variant="h4" sx={{ color: 'black' }} gutterBottom>
-            Contact Us
-          </Typography>
-          <Typography variant="h6" sx={{ color: '#black' }} gutterBottom>
-            We are here to help and answer any question you might have. We look forward to hearing from you.
-          </Typography>
-          <Grid container spacing={2} flexDirection={{
-            xs: "column",
-            sm: "column",
-            md: "row",
-          }}>
-            <Grid container p={1} xs={12} sm={12} md={6} flexDirection={"column"}>
-              <Grid item p={1}>
-                <TextField name="name" onChange={handleChange} fullWidth placeholder="Name" sx={textStyleObject} />
-              </Grid>
-              <Grid item p={1}>
-                <TextField name="email" onChange={handleChange} required fullWidth placeholder="Email" sx={textStyleObject} />
-              </Grid>
-              <Grid item p={1}>
-                <TextField name="subject" onChange={handleChange} fullWidth placeholder="Subject" sx={textStyleObject} />
-              </Grid>
-            </Grid>
-
-            <Grid container p={1} xs={12} sm={12} md={6} justifyContent={"center"}>
-              <Grid item xs={12} p={1}>
-                <TextField name="message" onChange={handleChange} fullWidth required placeholder="Message" multiline rows={4} sx={textStyleObject} />
-              </Grid>
-              <Grid container justifyContent={"center"} p={2}>
-                <Button variant='contained' sx={{
-                  backgroundColor: "orange", "&:hover": {
-                    backgroundColor: "green",
-                  }
-                }} type="submit">Send Message</Button>
-              </Grid>
-            </Grid>
-          </Grid>
-        </form>
-      </Grid>
-    </Grid>
-  )
-}
-
-export default Contact
+export default Contact;
