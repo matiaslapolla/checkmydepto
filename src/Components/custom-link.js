1ef3decd77c02a5ec0830910ad6ed50f9464bb46
Matias Lapolla <matias.lapolla.1@gmail.com>
2023-09-09 23:57:00 -0300

    updated menu styling

--- src/Components/custom-link.js
+++ src/Components/custom-link.js
@@ -1,34 +1,56 @@
-import { Grid, Link, Typography } from "@mui/material"
+import { Grid, Link, Typography } from '@mui/material';
 
 const CustomLink = (props) => {
-  return (
-    <Grid item xs={"auto"}>
-      <Link underline="none" href={props?.linkPath}>
-        {
-          props?.image ?
-            <img src={props?.image} alt={props?.linkTitle} style={{
-              height: "32px",
-              marginTop: "8px",
-            }} />
-            :
-            <Typography
-              sx={{
-                "&:hover": {
-                  color: "orange",
-                  cursor: "pointer",
-                  transition: "all 0.2s ease-in-out"
-                }
-              }}
-              color={"black"}
-              variant="body1"
-              fontWeight={"bold"}
-            >
-              {props?.linkTitle}
-            </Typography>
-        }
-      </Link>
-    </Grid>
-  )
-}
+	return (
+		<Grid
+			item
+			xs={'auto'}>
+			<Link
+				underline='none'
+				href={props?.linkPath}>
+				{props?.image ? (
+					<Grid
+						paddingY={1}
+						paddingX={2}>
+						<img
+							src={props?.image}
+							alt={props?.linkTitle}
+							style={{
+								height: '32px',
+								marginTop: '8px',
+							}}
+						/>
+					</Grid>
+				) : (
+					<Grid
+						paddingY={1}
+						paddingX={2}
+						borderRadius={'1rem'}
+						sx={{
+							'&:hover': {
+								backgroundColor: 'orange',
+								color: 'white',
+								transition: 'all 0.2s ease-in-out',
+							},
+						}}>
+						<Typography
+							sx={{
+								'&:hover': {
+									color: 'white',
+									transition: 'all 0.2s ease-in-out',
+								},
+							}}
+							color={'black'}
+							variant={'body1'}
+							fontWeight={'semibold'}
+							letterSpacing={0.6}>
+							{props?.linkTitle}
+						</Typography>
+					</Grid>
+				)}
+			</Link>
+		</Grid>
+	);
+};
 
-export default CustomLink
+export default CustomLink;
