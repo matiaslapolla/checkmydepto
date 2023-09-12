c3d2a29af629349d687adb9a2083adc30a56207c
Matias Lapolla <matias.lapolla.1@gmail.com>
2023-09-12 00:42:00 -0300

    refresh landing page

--- src/Components/hero-text-banner.js
+++ src/Components/hero-text-banner.js
@@ -1,46 +1,88 @@
-import { Grid, Typography } from '@mui/material'
-import React from 'react'
+import { Grid, Typography } from '@mui/material';
+import React from 'react';
 
 const HeroTextBanner = () => {
-  return (
-    <>
-      <Grid sx={{
-        gap: 2,
-        color: "white",
-        padding: 2,
-        zIndex: 122,
-        width: "100%",
-        minHeight: 170,
-        display: "flex",
-        textAlign: "center",
-        flexDirection: "column",
-        justifyContent: "center",
-      }}>
-        <Grid container gap={1} sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}>
-          <Grid item>
+	return (
+		<>
+			<Grid
+				sx={{
+					gap: 2,
+					color: 'white',
+					padding: 2,
+					zIndex: 122,
+					width: '100%',
+					height: '100%',
+					minHeight: 170,
+					display: 'flex',
+					textAlign: 'center',
+					flexDirection: 'column',
+					justifyContent: 'center',
+				}}>
+				<Grid
+					container
+					gap={1}
+					sx={{
+						display: 'flex',
+						justifyContent: 'center',
+						alignItems: 'center',
+					}}>
+					<Grid
+						item
+						width={{
+							xs: '100%',
+							sm: 'auto',
+						}}>
+						<Typography
+							xs={3}
+							color={'white'}
+							letterSpacing={1.5}
+							variant='h2'
+							fontWeight={'bold'}>
+							Welcome to{' '}
+						</Typography>
+					</Grid>
+					<Grid item>
+						<Typography
+							color={'white'}
+							letterSpacing={1.5}
+							fontWeight={900}
+							variant={'h2'}
+							sx={{
+								'&:hover': {
+									color: 'orange',
+									transition: 'all 0.2s ease-in-out',
+								},
+							}}>
+							CheckMyDepto
+						</Typography>
+					</Grid>
+				</Grid>
+				<Grid
+					sx={{
+						display: 'flex',
+						flexDirection: 'column',
+						justifyContent: 'center',
+						alignItems: 'center',
+					}}>
+					<Typography
+						color={'white'}
+						maxWidth={{
+							xs: '40ch',
+							sm: '75ch',
+						}}
+						variant='h6'>
+						Apartment reviews, personal recommendations, real photos
+						from the inside, and more!
+					</Typography>
+					<Typography
+						variant='h6'
+						color={'white'}>
+						Your #1 resource for apartment reviews
+					</Typography>
+				</Grid>
+			</Grid>
+		</>
+	);
+};
 
-            <Typography xs={3} variant="h3" fontWeight={"bold"}>Welcome to </Typography>
-          </Grid>
-          <Grid item>
-            <Typography xs={6} fontWeight={"bold"} variant={"h3"} sx={{ "&:hover": { color: "orange", transition: "all 0.2s ease-in-out" } }}>CheckMyDepto</Typography>
-          </Grid>
-        </Grid>
-        <Grid sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        }}>
-          <Typography sx={{ maxWidth: "75ch" }} variant="h6">Apartment reviews, personal recommendations, real photos from the inside, and more!</Typography>
-          <Typography variant="h6">Your #1 resource for apartment reviews</Typography>
-        </Grid>
-      </Grid>
-    </>
-  )
-}
-
-export default HeroTextBanner
+export default HeroTextBanner;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import '@fontsource/roboto/900.css';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+	<BrowserRouter>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	</BrowserRouter>
 );
